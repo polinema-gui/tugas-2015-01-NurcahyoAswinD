8d4b358ed155485d4646fe8ad8773e183cf3db57
Deni Aprianto <denniapr12@gmail.com>
2015-10-02 15:02:30 +0700

    Mengganti warna background

---
+++ MI2D-1431140080/MI2D-1431140080/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Dim b As Integer = 2
+    Private Sub Form1()
+        Me.BackColor = Color.Yellow
+    End Sub
+
+
+
+    Private Sub changeButton_Click(sender As Object, e As EventArgs) Handles changeButton.Click
+        If Me.BackColor = Color.Yellow Then
+            Me.BackColor = Color.Black
+        Else
+            Me.BackColor = Color.Yellow
+        End If
+    End Sub
+End Class
